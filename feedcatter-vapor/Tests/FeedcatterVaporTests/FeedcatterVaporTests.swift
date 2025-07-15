1279fbec44295d40a8ae4af31d30d16ab628b2fa
Markus Loide <markusloide@gmail.com>
2025-07-15 23:41:11 +0300

    Replace the tests with actual tests.

--- feedcatter-vapor/Tests/FeedcatterVaporTests/FeedcatterVaporTests.swift
+++ feedcatter-vapor/Tests/FeedcatterVaporTests/FeedcatterVaporTests.swift
@@ -1,11 +1,12 @@
-@testable import FeedcatterVapor
-import VaporTesting
-import Testing
 import Fluent
+import Testing
+import VaporTesting
+
+@testable import FeedcatterVapor
 
 @Suite("App Tests with DB", .serialized)
 struct FeedcatterVaporTests {
-    private func withApp(_ test: (Application) async throws -> ()) async throws {
+    private func withApp(_ test: (Application) async throws -> Void) async throws {
         let app = try await Application.make(.testing)
         do {
             try await configure(app)
@@ -19,63 +20,70 @@ struct FeedcatterVaporTests {
         }
         try await app.asyncShutdown()
     }
-    
-    @Test("Test Hello World Route")
-    func helloWorld() async throws {
-        try await withApp { app in
-            try await app.testing().test(.GET, "hello", afterResponse: { res async in
-                #expect(res.status == .ok)
-                #expect(res.body.string == "Hello, world!")
-            })
-        }
-    }
-    
-    @Test("Getting all the Todos")
-    func getAllTodos() async throws {
-        try await withApp { app in
-            let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
-            try await sampleTodos.create(on: app.db)
-            
-            try await app.testing().test(.GET, "todos", afterResponse: { res async throws in
-                #expect(res.status == .ok)
-                #expect(try res.content.decode([TodoDTO].self) == sampleTodos.map { $0.toDTO()} )
-            })
-        }
-    }
-    
-    @Test("Creating a Todo")
-    func createTodo() async throws {
-        let newDTO = TodoDTO(id: nil, title: "test")
-        
-        try await withApp { app in
-            try await app.testing().test(.POST, "todos", beforeRequest: { req in
-                try req.content.encode(newDTO)
-            }, afterResponse: { res async throws in
-                #expect(res.status == .ok)
-                let models = try await Todo.query(on: app.db).all()
-                #expect(models.map({ $0.toDTO().title }) == [newDTO.title])
-            })
-        }
-    }
-    
-    @Test("Deleting a Todo")
-    func deleteTodo() async throws {
-        let testTodos = [Todo(title: "test1"), Todo(title: "test2")]
-        
-        try await withApp { app in
-            try await testTodos.create(on: app.db)
-            
-            try await app.testing().test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
-                #expect(res.status == .noContent)
-                let model = try await Todo.find(testTodos[0].id, on: app.db)
-                #expect(model == nil)
-            })
-        }
-    }
-}
 
-extension TodoDTO: Equatable {
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id && lhs.title == rhs.title
+    @Test("pings the server")
+    func ping() async throws {
+        try await withApp { app in
+            try await app.testing().test(
+                .GET, "ping",
+                afterResponse: { res async in
+                    #expect(res.status == .ok)
+                    #expect(res.body.string == "pong")
+                })
+        }
+    }
+
+    @Test("creates a food")
+    func createFood() async throws {
+        try await withApp { app in
+            let createFoodDto = CreateFoodDTO(name: "Ookeanikala")
+            try await app.testing().test(
+                .POST, "foods",
+                beforeRequest: { req in
+                    try req.content.encode(createFoodDto)
+                },
+                afterResponse: { res async throws in
+                    #expect(res.status == .ok)
+                    let models = try await FoodModel.query(on: app.db).all()
+                    #expect(models.map({ $0.name }) == [createFoodDto.name])
+                })
+        }
+    }
+
+    @Test("deletes a food")
+    func deleteFood() async throws {
+        try await withApp { app in
+            let foodModel = FoodModel(
+                name: "Ookeanikala", state: DbFoodState.available, availablePercentage: 1.0)
+            try await foodModel.save(on: app.db)
+
+            try await app.testing().test(
+                .DELETE, "foods/\(foodModel.id!)",
+                afterResponse: { res async throws in
+                    #expect(res.status == .noContent)
+                    let models = try await FoodModel.query(on: app.db).all()
+                    #expect(models.count == 0)
+                })
+        }
+    }
+
+    @Test("lists foods")
+    func listsFoods() async throws {
+        try await withApp { app in
+            let foodModel = FoodModel(
+                name: "Ookeanikala", state: DbFoodState.available, availablePercentage: 1.0)
+            try await foodModel.save(on: app.db)
+            let foodModelTwo = FoodModel(
+                name: "Kana", state: DbFoodState.available, availablePercentage: 1.0)
+            try await foodModelTwo.save(on: app.db)
+
+            try await app.testing().test(
+                .GET, "foods",
+                afterResponse: { res async throws in
+                    #expect(res.status == .ok)
+                    let models = try await FoodModel.query(on: app.db).all()
+                    #expect(models.map { $0.name } == ["Ookeanikala", "Kana"])
+                })
+        }
     }
 }
